48e6b0cc29383be42de2b84b7d3470e9a0344f7e
h098s02922-beep <h098s02922@chunchon.gwe.hs.kr>
2025-11-26 11:18:25 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -71,7 +71,6 @@ CONTENT_BY_LABEL: dict[str, dict[str, list[str]]] = {
     labels[0]:{"texts":["중국식 냉면은 잘 모른다"], "videos":["https://youtu.be/JGPPYe7l6qE?si=-IsSf-eHv9QAs1L2"], "images":["https://share.google/images/tX7h2RfcFNvjFE9pZ"]},
     labels[1]:{"texts":["짜장면은 까맣다"], "videos":["https://youtu.be/sO8qdAuBAQs?si=Li0X5B-sDw2ZO263"], "images":["https://share.google/images/uph67fxL2YzEYulvl"]},
     labels[2]:{"texts":["짬뽕은 빨갛다"], "videos":["https://youtu.be/GP1AQDGYu2Q?si=dLT2K8Qp78uQZwkk"], "images":["https://share.google/images/qAKuLctl5VPTvoiqp"]},
-    labels[3]:{"texts":["탕수육의 탕은 설탕의 탕인거 앎?"], "videos":["https://youtube.com/shorts/Wloodma1iLY?si=dBqEU7c3rT_7GQ-e"], "images":["https://share.google/images/QbHbpNUKiVpI129vB"]}
 }
 
 # ======================
